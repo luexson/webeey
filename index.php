c41ad624140bd6cc28792bb9654f3479fb2411e7
luexson <luexson@gmail.com>
2021-06-21 14:21:37 +0800

    Twilio API
    Upload Twilio Stuff

--- index.php
+++ index.php
@@ -5,10 +5,46 @@
 </head>
 <body>
 
-<h1>Welcome to Webeey!</h1>
-<p>Modern VoIP over the Web</p>
+
     
-<p>TEST PUSH 1</p>
+<!-- START FORM -->    
+    
+<div class="col-sm-6 col-sm-offset-3" id="sms-container">
+
+  
+
+    <!-- OUR FORM -->
+    <form action="send_sms.php" method="POST">
+        
+        <!-- Call ID Number -->
+        <div id="cidnumber-group" class="form-group">
+            <input type="text" class="form-control" name="cidnumber" value="<?php echo "+1" . $effective_caller_id_number; ?>" readonly >
+            <!-- errors will go here -->
+        </div>
+
+        
+         <!-- DestinationNumber -->
+        <div id="number-group" class="form-group">
+            <input type="text" class="form-control" name="number" placeholder="To:">
+            <!-- errors will go here -->
+        </div>
+
+        <!-- Message -->
+        <div id="sms-group" class="form-group">
+            <input type="text" class="form-control" name="smessage" placeholder="Type a message...">
+            <!-- errors will go here -->
+        </div>
+
+
+        <button type="submit" class="btn btn-success">Send <span class="fa fa-arrow-right"></span></button>
+
+    </form>
+
+</div>
+    
+
+    
+<!-- END FORM -->    
 
 </body> 
 </html>
