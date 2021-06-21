d9585365b88a1a32b690f3552c5df1fb9f3f3baf
luexson <luexson@gmail.com>
2021-06-21 15:38:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,90 @@
 <html>
 <head>
 <title>Webeey - Telephony</title>
+    
+<!-- Start AJAXING -->
+<script>
+
+    $(document).ready(function() {
+
+    // process the form
+    $('form').submit(function(event) {
+        
+        $('.form-group').removeClass('has-error'); // remove the error class
+        $('.help-block').remove(); // remove the error text
+
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'smessage'              : $('input[name=smessage]').val(),
+            'number'                : $('input[name=number]').val(),
+            'cidnumber'             : $('input[name=cidnumber]').val()
+            
+        };
+        
+        // process the form
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'send_sms.php', // the url where we want to POST
+            data        : formData, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+                        encode          : true
+        })
+            // using the done promise callback
+            .done(function(data) {
+
+                // log data to the console so we can see
+                console.log(data);
+
+                // here we will handle errors and validation messages
+            
+                if ( ! data.success) {
+
+            // handle errors for name ---------------
+                        if (data.errors.smessage) {
+                            $('#sms-group').addClass('has-error'); // add the error class to show red input
+                            $('#sms-group').append('<div class="help-block">' + data.errors.smessage + '</div>'); // add the actual error message under our input
+                        }
+                       
+                       
+                        } else {
+                          // ALL GOOD! just show the success message!
+                          //$('form').html('<div class="alert alert-success">' + data.message + '</div>');
+                        
+                         $('#sms-group').removeClass('has-error').addClass('help-block'); // add the success class to show green                             
+                        $('#sms-group').append('<div class="help-block">' + data.message     + '</div>'); // add the actual error message under our input
+                            
+                        }
+
+            
+                //end ERROR HANDLING
+            });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
+
+});
+    
+    
+    /* SHORTHAND AJAX
+    
+        $.post('process.php', function(formData) {
+
+        // place success code here
+
+        })
+        .fail(function(data) {
+            // place error code here
+        });
+    
+    */
+    
+</script>
+    
+
+<!-- END AJAX -->
+    
 </head>
 <body>
 
